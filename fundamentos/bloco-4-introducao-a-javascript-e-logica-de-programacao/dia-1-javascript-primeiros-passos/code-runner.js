d2d2e333a4b69f57e3bc8d787fdd0709d03465d9
Regiane Santos <re.cfs235@gmail.com>
2021-07-28 18:39:55 -0300

    Agora a pratica ate o exercicio 3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/code-runner.js
@@ -0,0 +1,43 @@
+//console.log('Hello');
+//console.log('Javascript é maravilhoso');
+//console.log('Javascript é sensacional');
+
+// const team = 12;
+// let cohort = 15;
+
+// o sinal = está atribuindo o valor na variavel
+
+// tipos de primitivos: number, string, boolen, null, undefined
+
+let a=12;
+let b=10;
+
+console.log('Exercicio 1: ');
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+console.log('Exercicio 2 - Comparaçao de dois numeros:');
+if (a>b){
+console.log(a);
+} else {
+console.log(b);
+}
+
+
+let c=15;
+
+console.log('Exercicio 3 - Comparando 3 numeros: ')
+if (a>b && a>c){
+console.log(a);
+} else {
+    if (b>a && b>c){
+        console.log(b);
+    } else{
+        console.log(c);
+    }
+}
+
+
